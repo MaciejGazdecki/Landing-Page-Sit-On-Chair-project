9a6e5b1a1a6d88c7626207c6af11bfb50f3fa055
Maciej Gazdecki <maciej.gazdecki@gmail.com>
2019-06-20 00:05:10 +0200

    I adjusted image resolution and created a main banner with JS

--- js/main.js
+++ js/main.js
@@ -1,26 +1,38 @@
-(function showHidemenu() {
+(function mainPageAnimations() {
+    (function showHidemenu() {
 //IIFE which control showing and hiding small menu, I used ES6 arrow functions, which are shorter, and removed{}
-//because there is only one line of code. I put tohether query Selectors with EventListeners. I used forEach, to 
+//because there is only one line of code. I put tohether query Selectors with EventListeners. I used forEach, to
 // addEventListener to nodelist created by querySelectorAll.
-    const aboutCompanyMenu = document.querySelector('.company-menu');
-    document.querySelector('.about').addEventListener('mouseover', () => aboutCompanyMenu.classList.add('on'));
-    document.querySelector('.banner').addEventListener('mouseover', () => aboutCompanyMenu.classList.remove('on'));
-    document.querySelectorAll('.menu-items').forEach(function(menuItem) {
-        menuItem.addEventListener('mouseover', () => aboutCompanyMenu.classList.remove('on'))});
-})();
-(function showArrow() {
-    document.addEventListener('scroll', () => {
-        if(window.scrollY > 350) {
-            document.querySelector('.arrow-up').classList.remove('arrow-off')
-        } else {
-            document.querySelector('.arrow-up').classList.add('arrow-off')
-        }
-    })
-})();
-(function scrollTop() {
-    $('.arrow-up').on('click', () => {
-        $("html").animate({
-            scrollTop:0
+        const aboutCompanyMenu = document.querySelector('.company-menu');
+        document.querySelector('.about').addEventListener('mouseover', () => aboutCompanyMenu.classList.add('on'));
+        document.querySelector('.banner').addEventListener('mouseover', () => aboutCompanyMenu.classList.remove('on'));
+        document.querySelectorAll('.menu-items').forEach(function(menuItem) {
+            menuItem.addEventListener('mouseover', () => aboutCompanyMenu.classList.remove('on'))});
+    })();
+
+    (function showArrow() {
+        document.addEventListener('scroll', () => {
+            if(window.scrollY > 350) {
+                document.querySelector('.arrow-up').classList.remove('arrow-off')
+            } else {
+                document.querySelector('.arrow-up').classList.add('arrow-off')
+            }
         })
-    })
+    })();
+
+    (function scrollTop() {
+        $('.arrow-up').on('click', () => {
+            $("html").animate({
+                scrollTop:0
+            })
+        })
+    })();
+
+    (function switchImages() {
+        setInterval(() =>{
+            document.querySelector('.orange-chair').classList.toggle('image-off');
+            document.querySelector('.black-chair').classList.toggle('image-off');
+        },3500);
+    })();
 })();
+
