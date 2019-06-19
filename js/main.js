5b41ef0acfff09a1619ec967ba8f60471d35b4c5
GAZDECKI <Maciej.GAZDECKI@newellco.com>
2019-06-19 16:32:52 +0200

    I created JS function, which shows and hide about menu

---
+++ js/main.js
@@ -0,0 +1,10 @@
+(function showHidemenu() {
+//IIFE which control showing and hiding small menu, I used ES6 arrow functions, which are shorter, and removed{}
+//because there is only one line of code. I put tohether query Selectors with EventListeners. I used forEach, to 
+// addEventListener to nodelist created by querySelectorAll.
+    const aboutCompanyMenu = document.querySelector('.company-menu');
+    document.querySelector('.about').addEventListener('mouseover', () => aboutCompanyMenu.classList.add('on'));
+    document.querySelector('.banner').addEventListener('mouseover', () => aboutCompanyMenu.classList.remove('on'));
+    document.querySelectorAll('.menu-items').forEach(function(menuItem) {
+        menuItem.addEventListener('mouseover', () => aboutCompanyMenu.classList.remove('on'))});
+})();
