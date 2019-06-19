c9cf4e94f9fc0b5cf3f2966b0bc83d7d7c1bf969
Maciej Gazdecki <maciej.gazdecki@gmail.com>
2019-06-19 20:07:41 +0200

    I added animated arrow to scroll top

--- js/main.js
+++ js/main.js
@@ -8,3 +8,19 @@
     document.querySelectorAll('.menu-items').forEach(function(menuItem) {
         menuItem.addEventListener('mouseover', () => aboutCompanyMenu.classList.remove('on'))});
 })();
+(function showArrow() {
+    document.addEventListener('scroll', () => {
+        if(window.scrollY > 350) {
+            document.querySelector('.arrow-up').classList.remove('arrow-off')
+        } else {
+            document.querySelector('.arrow-up').classList.add('arrow-off')
+        }
+    })
+})();
+(function scrollTop() {
+    $('.arrow-up').on('click', () => {
+        $("html").animate({
+            scrollTop:0
+        })
+    })
+})();
