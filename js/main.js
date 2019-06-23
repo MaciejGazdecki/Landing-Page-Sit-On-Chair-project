d2ee0b03419514b4d25a89631bb224126fc23e6d
Maciej Gazdecki <maciej.gazdecki@gmail.com>
2019-06-23 16:56:46 +0200

    small touches and code formating

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 (function mainPageAnimations() {
     (function showHidemenu() {
-//IIFE which control showing and hiding small menu, I used ES6 arrow functions, which are shorter, and removed{}
-//because there is only one line of code. I put tohether query Selectors with EventListeners. I used forEach, to
-// addEventListener to nodelist created by querySelectorAll.
+        //IIFE which control showing and hiding small menu, I used ES6 arrow functions, which are shorter, and removed{}
+        //because there is only one line of code. I put tohether query Selectors with EventListeners. I used forEach, to
+        // addEventListener to nodelist created by querySelectorAll.
         const aboutCompanyMenu = document.querySelector('.company-menu');
         document.querySelector('.about').addEventListener('mouseover', () =>
             aboutCompanyMenu.classList.add('on'));
@@ -10,12 +10,13 @@
             aboutCompanyMenu.classList.remove('on'));
         document.querySelectorAll('.menu-items').forEach((menuItem) => {
             menuItem.addEventListener('mouseover', () =>
-                aboutCompanyMenu.classList.remove('on'))});
+                aboutCompanyMenu.classList.remove('on'))
+        });
     })();
 
     (function showScrollArrow() {
         document.addEventListener('scroll', () => {
-            if(window.scrollY > 350) {
+            if (window.scrollY > 350) {
                 document.querySelector('.arrow-up').classList.remove('arrow-off')
             } else {
                 document.querySelector('.arrow-up').classList.add('arrow-off')
@@ -26,16 +27,16 @@
     (function scrollTop() { // I used jQuery here
         $('.arrow-up').on('click', () => {
             $("html").animate({
-                scrollTop:0
+                scrollTop: 0
             })
         })
     })();
 
     (function switchImages() {
-        setInterval(() =>{
+        setInterval(() => {
             document.querySelector('.orange-chair').classList.toggle('image-off');
             document.querySelector('.black-chair').classList.toggle('image-off');
-        },3500);
+        }, 3500);
     })();
 })();
 
